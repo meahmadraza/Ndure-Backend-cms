efc5c495d98ae670f7c9f252f590ce269db0081e
Ahmad Raza <ahmadsajidmughal357@gmail.com>
2025-05-23 18:45:18 +0500

    first commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+export default {
+    upload: {
+        config: {
+            breakpoints: {
+                thumbnail: 156,
+                small: 470,
+                medium: 1024,
+                large: 1600,
+            },
+        },
+    },
+};
